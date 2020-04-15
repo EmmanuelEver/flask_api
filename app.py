7c0ef16fc1ca335e9e6b0dde68e738f4982fd1bc
OJT1 - Emman <ojt1@fligno.com>
2020-04-16 00:04:46 +0800

    added heroku-postgres db

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,7 +12,7 @@ app  			 = Flask(__name__)
 app.secret_key	 = "dalbong"
 api 		     = Api(app)
 jwt 		     = JWT(app, authenticate, identity)
-app.config['SQLALCHEMY_DATABASE_URI'] 			= 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] 			= os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']    = False
 app.config['JWT_EXPIRATION_DELTA'] 				= timedelta(seconds =1800)
 
