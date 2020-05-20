e536568e2ccd152f7a8c196a936f3fa877a07a0c
OJT1 - Emman <ojt1@fligno.com>
2020-05-20 15:02:22 +0800

    fix some problems with the login resource

--- resources/user.py
+++ resources/user.py
@@ -69,7 +69,8 @@ class UserLogin(Resource):
 				refresh_token = create_refresh_token(user.id)
 				return {
 						"access_token" : access_token,
-						"refresh_token" : refresh_token
+						"refresh_token" : refresh_token,
+						"user_id"		:user.id
 					}, 201
 
 			return {"message":"Invalid Crederntials"}, 401
